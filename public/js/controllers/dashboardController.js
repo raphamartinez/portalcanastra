c8bc7de42cbafd602eb0132ba2a92e41c10f9ad9
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2021-05-18 23:26:02 -0300

    commit

--- public/js/controllers/dashboardController.js
+++ public/js/controllers/dashboardController.js
@@ -17,7 +17,7 @@ window.onload = async function () {
 
     let cardHistory = document.querySelector('[data-card]')   
 
-    let dividlogin = document.querySelector('[data-id_login-sy]').value
+    let dividlogin = document.querySelector('[data-id_login-sy]')
 
 
     let history = await Service.historyDashoard()
