b960256dfa7a9f36bc07a8df459a89414414d25a
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2021-06-08 20:47:34 -0300

    commit

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ connection.connect((error => {
                 res.sendFile(__dirname + '/views/public/login.html');
             });
 
-            WebScraping.init()
-
             const job = new CronJob('0 01 * * * *', () => {
                 try{
                     console.log('Executed Cron sucessfuly!');
