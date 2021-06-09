bf8abf11ac38e113efcf7499953eddb99b458661
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2021-06-09 16:49:39 -0300

    commit

--- api/infrastructure/database/connectionhbs.js
+++ api/infrastructure/database/connectionhbs.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const connectionhbs = mysql.createConnection({
-  user: process.env.DBHBS_USER,
-  host: process.env.DBHBS_HOST,
-  database: process.env.DBHBS_NAME,
-  password: process.env.DBHBS_PASSWORD,
-  port: process.env.DBHBS_PORT
+  user: 'dblectura',
+  host: '45.232.214.13',
+  database: 'ansa',
+  password: 'Dblec#2020',
+  port: '63306'
 })
 
 module.exports = connectionhbs
