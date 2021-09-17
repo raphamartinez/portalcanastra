aaecbc0db977e82d90245b047b4e6455bdc98541
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2021-09-17 18:41:41 -0300

    commit

--- index.js
+++ index.js
@@ -2,64 +2,62 @@ require('dotenv').config({ path: __dirname + '\\.env', encoding: 'utf8' })
 require('events').EventEmitter.prototype._maxListeners = 100;
 
 const customExpress = require('./api/config/customExpress')
-const connection = require('./api/infrastructure/database/connection')
-const tables = require('./api/infrastructure/database/tables')
 const express = require('express')
 const path = require('path')
+const jwt = require('jsonwebtoken');
+const { InvalidArgumentError, NotFound, NotAuthorized } = require('./api/models/error');
+// const connection = require('./api/infrastructure/database/connection')
+// const tables = require('./api/infrastructure/database/tables')
 
-connection.connect((error => {
-    if (error) {
-        console.log(error)
-    } else {
-        const app = customExpress()
-        tables.init(connection)
-        app.listen(3000, () => {
-            console.log(`working!`);
 
-            app.set('views', [path.join(__dirname, 'views/public'), path.join(__dirname, 'views/admin')])
-            app.engine('html', require('ejs').renderFile)
-            app.set('view engine', 'html')
+process.setMaxListeners(100)
+const app = customExpress()
 
-            app.use(express.static(__dirname + '/public'))
-            app.use(express.static(__dirname + '/views'))
+app.set('views', [path.join(__dirname, 'views/public'), path.join(__dirname, 'views/admin')])
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
 
-            app.get('/', function (req, res) {
-                res.render('login');
-            });
+app.listen(3000, () => {
+    console.log(`working!`);
 
-            app.use((err, req, res, next) => {
+    app.use(express.static(__dirname + '/public'))
+    app.use(express.static(__dirname + '/views'))
 
-                let status = 500
-                const body = {
-                    message: err.message
-                }
+    app.get('/', function (req, res) {
+        res.render('login');
+    });
 
-                if (err instanceof NotFound) {
-                    status = 404
-                    body.dateExp = err.dateExp
-                }
+    app.use((err, req, res, next) => {
 
-                if (err instanceof NotAuthorized) {
-                    status = 401
-                    body.dateExp = err.dateExp
-                }
+        let status = 500
+        const body = {
+            message: err.message
+        }
 
-                if (err instanceof InvalidArgumentError) {
-                    status = 400
-                }
+        if (err instanceof NotFound) {
+            status = 404
+            body.dateExp = err.dateExp
+        }
 
-                if (err instanceof jwt.JsonWebTokenError) {
-                    status = 401
-                }
+        if (err instanceof NotAuthorized) {
+            status = 401
+            body.dateExp = err.dateExp
+        }
 
-                if (err instanceof jwt.TokenExpiredError) {
-                    status = 401
-                    body.dateExp = err.dateExp
-                }
+        if (err instanceof InvalidArgumentError) {
+            status = 400
+        }
 
-                res.status(status)
-                res.json(body)
-            })
-        })
-    }
-}))
+        if (err instanceof jwt.JsonWebTokenError) {
+            status = 401
+        }
+
+        if (err instanceof jwt.TokenExpiredError) {
+            status = 401
+            body.dateExp = err.dateExp
+        }
+
+        res.status(status)
+        res.json(body)
+    })
+})
